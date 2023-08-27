a8b00a5e16caa29b3190d5d8d625d3a04a4d6b59
nekiuser <merkasivan91@gmail.com>
2023-08-27 23:38:59 +0200

    Projektni zadatak 1 PHP i MySQL

---
+++ Projetkni_zadatak_PHP_MySQL_1/aboutus.php
@@ -0,0 +1,16 @@
+<?php
+print'
+    <h1>About Us</h1>
+    <h2>Save water and time</h2>
+    <video width="40%" controls poster="img/video_cover.jpg" >
+    <source src="video/Smart_watering.mp4" type="video/mp4">
+    Your browser does not support HTML5 video.
+    </video>
+    <p>Adjustable drippers save up to 70% of water with drip irrigation. By dripping water, you will save time because classic pouring is not necessary - use your free time for other things.</p>
+    <p>The Micro-Drip-System with precise accuracy delivers water evenly exactly where the plants need it most, the roots.</p>
+    <p>The Micro-Drip-System gives you the necessary flexibility to create an individual system according to your needs. Patented "Quick and easy" pipe connection technology and a wide range help you in installation and system conversion.</p>
+    <p>The Micro-Drip-System makes you independent of helping your neighbors during your absence. There is no need to give watering instructions and fear that your plants will not receive optimal care.</p>
+    <p>Video courtesy of <a href="https://www.gardena.com/hr/c/otkrijte/odrzivost-drustvena-odgovornost/ekonomicno-navodnjavanje" target="_blank">gardena.com</a></p>';
+?>
+                  
+    
---
+++ Projetkni_zadatak_PHP_MySQL_1/contact.php
@@ -0,0 +1,34 @@
+<?php
+print'
+     <div class="Contact form">
+        <h1>Contact Form</h1>
+            <form action="Contact form">
+                <div class="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2781.797289129728!2d15.966938676743391!3d45.79528871128627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4765d68b441ce2df%3A0x54e2a03adf42446f!2sTehni%C4%8Dko%20veleu%C4%8Dili%C5%A1te%20u%20Zagrebu!5e0!3m2!1shr!2sus!4v1692631198737!5m2!1shr!2sus" width="100%" height="650px" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+              <div style="margin-top:50px;"></div>
+              <label for="fname">First Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="Your name..">
+          
+              <label for="lname">Last Name</label>
+              <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+          
+              <label for="email">E-mail</label>
+              <input type="text" id="email" name="email" placeholder="Your E-mail..">
+
+              <label for="country">Country</label>
+              <select id="country" name="country">
+                <option value="">Please select</option>
+                <option value="australia">Australia</option>
+                <option value="canada">Canada</option>
+                <option value="usa">USA</option>
+                <option value="hr" selected>Croatia</option>
+              </select>
+          
+              <label for="subject">Subject</label>
+              <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+          
+              <input type="submit" value="Submit">
+           </form>
+     </div> ;'
+?>
